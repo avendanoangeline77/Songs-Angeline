5d481193f630eebab287b0038ea656bf227a0f2c
avendanoangeline77 <uba.avendanoangelina@gmail.com>
2024-06-03 12:06:12 -0300

    falta completar algunas cosas

--- canciones/bandas.py
+++ canciones/bandas.py
@@ -1,19 +1,33 @@
-from flask import Flask, render_template, Blueprint
+from flask import Blueprint, render_template
 from . import db
 
 bp = Blueprint("bandas",__name__, url_prefix="/bandas")
 
+@bp.route('/<init:id>')
+def detalles(id):
+    con= db.get_db()
+    consulta1 = """
+      SELECT name FROM artists WHERE  ArtistId = ?;
+    """
+    consulta2 = """
+    SELECT Title, AlbumId FROM albums WHERE ArtistId = ?; 
+    """
+    res = con.execute(consulta1, (id,))
+    artistas = res.fetchone()
+    res = con.execute(consulta2, (id,))
+    lista_canciones = res.fetchall()
+    pagina = render_template('detallesCanciones.html'
+                             artistas=artistas,
+                             canciones = lista_canciones)
+    return pagina
+
 @bp.route('/')
-def canciones():
-    consulta_bandas = """
+def artists():
+    data_base = db.get_db()
+    ask = """
           select name from artists 
-
         """
-
-    base_de_datos = db.get_db()
-
-    resultado = base_de_datos.execute(consulta_bandas)
-    lista_de_bandas = resultado.fetchall()
+    bandas = data_base.execute(ask)
+    lista_de_bandas = bandas.fetchall()
 
     pagina = render_template("bandas.html",  bandas = lista_de_bandas)
-    return pagina

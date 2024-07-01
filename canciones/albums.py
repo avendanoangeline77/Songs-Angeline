76c0ba5e9eddb37bdd1db88a0a7ac393405841a0
avendanoangeline77 <uba.avendanoangeline77>
2024-07-01 10:42:05 -0300

    agregue la ruta de detalles para que muestre los albums de determinado artista

--- canciones/albums.py
+++ canciones/albums.py
@@ -17,3 +17,26 @@ def canciones():
 
     pagina = render_template("album.html",  albums = lista_de_albums)
     return pagina
+
+@bp.route('/<int:id>')
+def detalles(id): 
+     consulta_albums = """
+      select Title from albums
+       WHERE AlbumId = ?
+     """
+     consulta_detalle_albums = """
+       select t.Name, a.Title from albums a
+       JOIN tracks t on a.AlbumId = t.AlbumId
+       WHERE a.AlbumId = ? ;
+     """
+
+     base_de_datos = db.get_db()
+     resultado = base_de_datos.execute(consulta_albums, (id,))
+     albums = resultado.fetchall()
+
+     base_de_datos = db.get_db()
+     resultado = base_de_datos.execute(consulta_detalle_albums, (id,))
+     lista_de_albums = resultado.fetchall()
+
+     pagina = render_template("detalleAlbums.html", album = albums, albums = lista_de_albums)
+     return pagina
